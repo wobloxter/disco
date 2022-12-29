fb9dfb316c84c253fbd35936ff5264f23ce599dc
wobloxter <121632128+wobloxter@users.noreply.github.com>
2022-12-29 20:13:25 +0000

    Added disco

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+basic.showString("LETS DO A DISCO :)")
+basic.showIcon(IconNames.Happy)
+basic.pause(2000)
+basic.showLeds(`
+    # . # . #
+    # . # . #
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+basic.pause(2000)
+basic.showString("3....2....1...GO")
+basic.forever(function () {
+    cuteBot.motors(100, 0)
+    basic.pause(1000)
+    cuteBot.motors(0, 100)
+    basic.pause(1000)
+})
+basic.forever(function () {
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+    basic.pause(100)
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff8000)
+    basic.pause(100)
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xffff00)
+    basic.pause(100)
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
+    basic.pause(100)
+})
 basic.forever(function () {
 	
 })
